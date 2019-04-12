7e7fcc11991c2231fb4331f271e202e4a8d0973a
Iskra B <ibatistapoblete@gmail.com>
2019-04-12 01:59:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,3 +15,13 @@ def take_a_number (array, name)
   array << name
   puts "Welcome, #{name}. You are number #{array.length} in line."
 end
+
+def now_serving(array)
+  if array.empty?
+    puts "There's nobody waiting to be served!"
+  else
+    array.each do |name| puts "Currently serving: #{name}."
+      array.pop
+    end
+  end
+end
