fdbfbfb476c8b29dd97077a7f1e33a4193b05a8e
Iskra B <ibatistapoblete@gmail.com>
2019-04-12 01:18:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,9 @@
 katz_deli = []
 
 def line(katz_deli)
-  katz_deli.map.with_index do |name, index| puts "The line is currently: #{index+1}. #{name}"
+  katz_deli.map.with_index do |name, index| 
+    puts "The line is currently: #{index+1}. #{name}"
     end
-  else
     puts "The line is currently empty."
   end
 end
