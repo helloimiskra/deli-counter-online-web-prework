9c706fe73dbee58c5cbc8ccee171e0d4deb4f095
Iskra B <ibatistapoblete@gmail.com>
2019-04-12 01:03:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ katz_deli = []
 def line(array)
   if array.length > 0
     puts "The line is currently:"
-  array.each_with_index.map do |name, index| print "#{index+1}. #{name}"
+  array.each_with_index.map do |name, index| puts "#{index+1}. #{name}"
     name += 1
     index += 1
     end
