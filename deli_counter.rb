5f55646e5986a1e1e9c2493d7dab9e2af232e8d9
Iskra B <ibatistapoblete@gmail.com>
2019-04-12 01:35:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,23 +1,7 @@
 katz_deli = []
 
 def line(array)
-  array.each_with_index.map do |name, index|
-    print "The line is currently: #{index+1}. #{name}"
-  end
-  return "The line is currently empty."
-end
-
-def take_a_number(array, name)
-  array = []
-  array.each do |name| new_line << puts "#{name}"
-  end
-  new_line.each_with_index do |name, index|
-    index = index+1
-    puts "Welcome, #{name}. You are number #{index} in line."
-  end
-end
-
-def now_serving(katz_deli)
-  array.each do |name| "Currently serving: #{name}"
-  end
-end
+  if array.empty?
+    puts "The line is currently empty."
+  else
+  
