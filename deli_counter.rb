ade456368f0b3a35228bc73a6ce27f333c441bee
Iskra B <ibatistapoblete@gmail.com>
2019-04-12 01:40:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(array)
     puts "The line is currently empty."
   else
     new_array = "The line is currently:"
-    array.each_with_index do |name, index| new_array << "#{index+1}. #{name}"
+    array.each_with_index do |name, index| new_array << " #{index+1}. #{name}"
     end
     puts new_array
   end
