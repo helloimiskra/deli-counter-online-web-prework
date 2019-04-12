5d34a9a01e92561c7698448ae7b2dda32bbe4825
Iskra B <ibatistapoblete@gmail.com>
2019-04-12 01:02:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,10 @@ katz_deli = []
 
 def line(array)
   if array.length > 0
-  array.each_with_index.map do |name, index| print "The line is currently: #{index+1}. #{name} #{index+1}. #{name} #{index+1}. #{name}"
+    puts "The line is currently:"
+  array.each_with_index.map do |name, index| print "#{index+1}. #{name}"
+    name += 1
+    index += 1
     end
   else
     puts "The line is currently empty."
