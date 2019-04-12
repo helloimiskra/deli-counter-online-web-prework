cc59e52259d1540e17c7ac521edfe9857a28ee1c
Iskra B <ibatistapoblete@gmail.com>
2019-04-12 02:00:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,7 +18,7 @@ end
 
 def now_serving(array)
   if array.empty?
-    puts "There's nobody waiting to be served!"
+    puts "There is nobody waiting to be served!"
   else
     "Currently serving #{array.first}."
     array.shift
