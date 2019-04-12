4528de7490ac0f16c1b6ef98ae2c4f07203ba828
Iskra B <ibatistapoblete@gmail.com>
2019-04-12 01:21:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,6 @@ def line(array)
   array.each_with_index do |name, index|
     puts "The line is currently: #{index+1}. #{name}"
     end
-    puts "The line is currently empty."
+    return "The line is currently empty."
   end
 end
